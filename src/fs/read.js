208c4d6bbd29478979888f8930cf33c4a803a9bb
Ina Litvinka <inlibitum@gmail.com>
2025-10-22 20:30:04 +0200

    refactor(fs): remove commented-out code & unused imports

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,8 +1,6 @@
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { readFile } from 'fs/promises';
-import { createReadStream } from 'fs';
-import { stdout } from 'process';
 
 const FOLDER_NAME = 'files';
 const FILE_TO_READ = 'fileToRead.txt';
@@ -21,15 +19,6 @@ const read = async () => {
     const errorMessage = error.code === ERROR_CODE ? ERROR_MESSAGE : error.message;
     throw new Error(errorMessage);
   }
-//   const stream = createReadStream(filePath, 'utf-8');
-//   await new Promise((resolve, reject) => {
-//     stream.on('data', (chunk) => stdout.write(chunk));
-//     stream.on('end', () => {
-//     stdout.write('\n \n');
-//     resolve();
-// });
-//     stream.on('error', (error) => reject(new Error(ERROR_MESSAGE)));
-//   })
 };
 
 await read();
