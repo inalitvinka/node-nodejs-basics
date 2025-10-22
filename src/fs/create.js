20bcc01f273d9a2914825733cda8f9f5e617eaa5
Ina Litvinka <inlibitum@gmail.com>
2025-10-22 15:24:04 +0200

    feat(fs): implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,33 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { writeFile, readFile, access } from 'fs/promises';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fresh.txt';
+const FILE_CONTENT = 'I am fresh and young';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-  // Write your code here
+  const pathToFile = join(__dirname, FOLDER_NAME, FILE_NAME)
+  try {
+    await writeFile(pathToFile, FILE_CONTENT, { flag: 'wx' });
+  } catch (error) {
+    console.log(error)
+    throw new Error(ERROR_MESSAGE);
+  }
+  // try {
+  //   await access(pathToFile);
+  //   throw new Error(ERROR_MESSAGE);
+  // } catch (error) {
+  //   if (error.code === 'ENOENT') {
+  //     await writeFile(pathToFile, FILE_CONTENT);
+  //   } else {
+  //     throw error;
+  //   }
+  // }
 };
 
 await create();
