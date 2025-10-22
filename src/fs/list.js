97ce9541e210794b1fabdbcd038f0d058d79d1d9
Ina Litvinka <inlibitum@gmail.com>
2025-10-22 20:46:13 +0200

    feat(fs): implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { readdir } from 'fs/promises';
+
+const FOLDER_NAME = 'files';
+const ERROR_MESSAGE = 'FS operation failed';
+const ERROR_CODE = 'ENOENT';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+
 const list = async () => {
-  // Write your code here
+  const pathToFile = join(dirName, FOLDER_NAME);
+  try {
+    const files = await readdir((pathToFile));
+    console.log(files);
+  } catch (error) {
+    const errorMessage = error.code === ERROR_CODE ? ERROR_MESSAGE : error.message;
+    throw new Error(errorMessage);
+  }
 };
 
 await list();
