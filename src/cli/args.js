b9ec43874e61fb55b50da3b83d6ff2f756e2acc0
Ina Litvinka <inlibitum@gmail.com>
2025-10-24 15:41:18 +0200

    refactor(cli): avoid magic numbers

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,7 +1,10 @@
+const PFEFIX_LENGTH = 2;
+const ARGS_STEP = 2;
+
 const parseArgs = () => {
-  const args = process.argv.slice(2);
+  const args = process.argv.slice(PFEFIX_LENGTH);
   const result = [];
-  for (let i = 0; i < args.length; i += 2) {
+  for (let i = 0; i < args.length; i += ARGS_STEP) {
     result.push(`${args[i].slice(2)} is ${args[i + 1]}`);
   }
   console.log(result.join(', '));
