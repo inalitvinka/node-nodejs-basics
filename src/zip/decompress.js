32c668c05d1fc51db5ce2c41b5a5a01cd60cbc77
Ina Litvinka <inlibitum@gmail.com>
2025-10-25 23:43:00 +0200

    feat(zip): implement decompress function using zlib and Streams API

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+
+const FOLDER_NAME = 'files';
+const SOURCE_FILE = 'archive.gz';
+const DEST_FILE = 'fileToCompress.txt';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+const pathToSrc = join(dirName, FOLDER_NAME, SOURCE_FILE);
+const pathToDest = join(dirName, FOLDER_NAME, DEST_FILE); 
+
 const decompress = async () => {
-  // Write your code here
+  const srcStream = createReadStream(pathToSrc);
+  const destStream = createWriteStream(pathToDest);
+  const gunzip = createGunzip();
+  try {
+    await pipeline(srcStream, gunzip, destStream);
+    console.log('Decompression complete.')
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await decompress();
