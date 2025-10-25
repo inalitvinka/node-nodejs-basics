7c3d5e7ad44f3c9a7d2fb7681868d3ed5993a0ee
Ina Litvinka <inlibitum@gmail.com>
2025-10-25 17:11:53 +0200

    refactor(streams): remove a previous solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -8,7 +8,6 @@ import { EOL } from 'os';
 
 const FOLDER_NAME = 'files';
 const FILE_NAME = 'fileToRead.txt';
-const LINE_FEED = '\n';
 
 const fileName = fileURLToPath(import.meta.url);
 const dirName = dirname(fileName);
@@ -16,12 +15,6 @@ const dirName = dirname(fileName);
 const read = async () => {
   const pathToFile = join(dirName, FOLDER_NAME, FILE_NAME);
   const stream = createReadStream(pathToFile);
-  // stream.pipe(stdout);
-  // await new Promise((resolve, reject) => {
-  //   stream.on('end', resolve);
-  //   stream.on('error', reject);
-  // });
-  // stdout.write(LINE_FEED);
   try {
     await pipeline(stream, stdout, { end: false });
     stdout.write(EOL);
