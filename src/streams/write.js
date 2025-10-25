48bbfd95b5b61438a82756f92343a894919babde
Ina Litvinka <inlibitum@gmail.com>
2025-10-25 17:51:57 +0200

    feat(streams): implement write function using writable stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createWriteStream } from 'fs';
+import { stdin } from 'process';
+import { pipeline } from 'stream/promises';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fileToWrite.txt';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+
 const write = async () => {
-  // Write your code here
+  const pathToFile = join(dirName, FOLDER_NAME, FILE_NAME);
+  try {
+    await pipeline(stdin, createWriteStream(pathToFile))
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await write();
