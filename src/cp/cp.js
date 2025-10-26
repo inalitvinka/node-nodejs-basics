0f32c9418c2a9db6aa4e9ebbf9e2258d57baf230
Ina Litvinka <inlibitum@gmail.com>
2025-10-26 20:02:00 +0100

    feat(cp): implement spawnChildProcess in cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,30 @@
+import { fork } from 'child_process';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'script.js';
+const ERROR_MESSAGE = 'Error:';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+const pathToFile = join(dirName, FOLDER_NAME, FILE_NAME);
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const child = fork(pathToFile, args, {
+    stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+  });
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+
+  child.on('error', (error) => {
+    console.error(ERROR_MESSAGE, error);
+  });
+  
+  child.stderr.on('data', (chunk) => {
+    console.error(ERROR_MESSAGE, chunk.toString());
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['Lamborghini', 'Ferrari', 'Maserati', 'Rolls-Royce'] );
